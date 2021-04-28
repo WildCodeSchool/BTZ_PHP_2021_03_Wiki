ab296a8f419e59ef0800c08f7e5ed0c1d815f85a
lolaonthemoon <laurence.idiart@gmai.com>
2021-04-28 18:54:55 +0200

    creation du CRUD Category

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+// used to control entries validity in the buildForm function
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+         // LI add article is commented : not necessary in our case  //
+         // ->add('articles')
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
