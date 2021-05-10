56f79db5624fd154735e875000bf7d3e1a690c18
Vanessa Salvador <salvadoribanez.vanessa@gmail.com>
2021-05-10 16:53:53 +0200

    Added label names in french into user form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,17 +7,20 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'label' => 'Email'
+            ])
             ->add('roles', ChoiceType::class, [
                 'choices' => [
                     'Utilisateur' => 'ROLE_USER',
@@ -25,13 +28,14 @@ class UserType extends AbstractType
                     'Administrateur' => 'ROLE_ADMIN'
                 ],
                 'expanded' => true,
-                'multiple' => true ,
-                'label' => 'Rôles' 
+                'multiple' => true,
+                'label' => 'Rôle'
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
+                'label' => 'Mot de passe',
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -44,10 +48,15 @@ class UserType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('firstname')
-            ->add('lastname')
-            ->add('cityAgency')
-        ;
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('cityAgency', TextType::class, [
+                'label' => 'Ville'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
