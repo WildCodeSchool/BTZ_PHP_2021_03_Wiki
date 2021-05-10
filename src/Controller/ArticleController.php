b310cb352ac0e81e39347e401f0cd68b075b4de7
Vanessa Salvador <salvadoribanez.vanessa@gmail.com>
2021-05-10 12:14:01 +0200

    Fixes on the articles page and in the ArticleController

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Entity\Version;
 use App\Form\ArticleType;
-use App\Repository\ArticleRepository;
 use App\Repository\VersionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
