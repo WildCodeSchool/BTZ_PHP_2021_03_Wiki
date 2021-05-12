a2ecd476f6aef9a613dc741b0acd41aa7115a22f
lolaonthemoon <laurence.idiart@gmai.com>
2021-05-12 13:43:42 +0200

    correction translation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"email"}, message="Ce mot de passe est déjà utilisé par un collaborateur du Wiki")
+ * @UniqueEntity(fields={"email"}, message="Cet email est déjà utilisé par un collaborateur du Wiki")
  */
 class User implements UserInterface
 {
