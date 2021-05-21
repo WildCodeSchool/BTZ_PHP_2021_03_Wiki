d9cb8f08b4e4d6f6f07c740bbc28812db0c0b0b3
lolaonthemoon <laurence.idiart@gmai.com>
2021-05-21 10:18:17 +0200

    juste pour le pull

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -8,9 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\VersionRepository;
-use Knp\Component\Pager\PaginatorInterface; // Nous appelons le bundle KNP Paginator
+use Knp\Component\Pager\PaginatorInterface;
 
+// Nous appelons le bundle KNP Paginator
 /**
  * @Route("/category")
  */
@@ -102,5 +102,4 @@ class CategoryController extends AbstractController
 
         return $this->redirectToRoute('category_index');
     }
-
 }
